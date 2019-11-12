705d38e806ada912d07c9750abec92ee6e94917b
Max <maaxx.360@gmail.com>
2019-11-12 01:15:42 -0600

    Create

--- PIA/create.c
+++ PIA/create.c
@@ -17,8 +17,7 @@ int main()
 {
     srand(time(NULL));
     int i;
-    int counter=12345;
-    int no_list=1;
+    int no_list = 1;
     struct cliente cl;
     FILE *fp, *nm;
     fp = fopen("db.txt", "w+");
@@ -29,20 +28,20 @@ int main()
         fscanf(nm, "%s %s", cl.nombre, cl.apellido);
         cl.no_cliente = 1000 + (rand() % 1000);
         cl.pswd = 1111;
-        cl.balance = (float) (rand() % 10000);
+        cl.balance = (float)(rand() % 10000);
         do
         {
             cl.no_transfer = rand() % 10000;
         } while (cl.no_transfer < 1000);
         cl.no_list = no_list;
         no_list++;
-        counter++;
         fprintf(fp, "%d %d %s %s %d %f %d\n", cl.no_list, cl.no_cliente, cl.nombre, cl.apellido, cl.pswd, cl.balance, cl.no_transfer);
     }
     rewind(fp);
     // fseek(fp, sizeof(struct cliente), SEEK_SET);
     // printf("%ld", ftell(fp));
-    for(i=0;i<10;i++){
+    for (i = 0; i < 10; i++)
+    {
         fscanf(fp, "%d %d %s %s %d %f %d\n", &cl.no_list, &cl.no_cliente, cl.nombre, cl.apellido, &cl.pswd, &cl.balance, &cl.no_transfer);
         printf("%s %s\nCliente: %d Contrasena: %d\nBalance: %.2f No_transfer: %d No_list: %d\n\n", cl.nombre, cl.apellido, cl.no_cliente, cl.pswd, cl.balance, cl.no_transfer, cl.no_list);
     }
